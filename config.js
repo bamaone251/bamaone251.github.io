447a9e71b57b9fdc44ca4d687c15ed2285235ae2
bamaone251 <63915412+bamaone251@users.noreply.github.com>
2024-10-31 20:54:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const aURL = [
   ["add10d", "BACK", "#", "1"],
   ["add10d", "BACK", "#", "1", "R"],
   ["ff9100", "Refresh", "#", "1"],
-  
+
   ["2196F3", "WINDY", "https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=%C2%B0F&metricWind=default&zoom=5&overlay=radar&product=radar&level=surface&lat=31.062&lon=-90.956", "1"],
   [
     "2196F3",
@@ -18,11 +18,10 @@ const aURL = [
   [
     "2196F3",
     "RADAR",
-    "https://weather.cod.edu/satrad/nexrad/?parms=MOB-N0B-0-00-000",
+    "https://www.youtube.com/live/O9mYwRlucZY?si=GlU95eSlWX82YhNw",
     "1",
-    "L"
   ],
-  ["2196F3", "DX CLUSTER", "https://www.nnvl.noaa.gov/NESDIS/site/videos/GOES/USlamWV-sector.mp4?_ga=2.214018519.1525759656.1717948358-643504581.1714231811", "1","R"],
+  ["2196F3", "DX CLUSTER", "https://www.nnvl.noaa.gov/NESDIS/site/videos/GOES/USlamWV-sector.mp4?_ga=2.214018519.1525759656.1717948358-643504581.1714231811", "1", "R"],
   [
     "2196F3",
     "LIGHTNING",
@@ -30,7 +29,7 @@ const aURL = [
     "1",
     "R",
   ],
-  
+
   ["2196F3", "WATER VAPOR", "https://cw3e.ucsd.edu/ivt_iwv_namerica/", '1'],
 
   ["2196F3", "CURRENTS", "https://earth.nullschool.net/#current/ocean/surface/currents/orthographic=-71.31,25.84,793/loc=-68.845,31.7940", "1", "R"],
@@ -59,35 +58,57 @@ const aURL = [
 const aIMG = [
   ["", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
   [
-    "",
-    "https://youtu.be/N5wWOVOTHmQ",
+    "WARNINGS",
+    "https://climate.cod.edu/data/satellite/regional/southcentral/current/southcentral.ww.gif",
   ],
-  [
-    "",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/GOES16-CONUS-GEOCOLOR-625x375.gif",
-  ],
-  ["10M PROPAGATION", "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/CAR/Sandwich/GOES16-CAR-Sandwich-1000x1000.gif"],
-  [
-    "",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/SP/Sandwich/GOES16-SP-Sandwich-600x600.gif",
-  ],
-  [
-    "",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/GM/Sandwich/GOES16-GM-Sandwich-1000x1000.gif", ".8",
-  ],
+
   [
     "LIGHTNING",
     "https://images.lightningmaps.org/blitzortung/america/index.php?animation=usa",
   ],
+
+
   [
-    "WARNINGS",
-    "https://climate.cod.edu/data/satellite/regional/southcentral/current/southcentral.ww.gif",
+    "THE SUN",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/SUVI/FD/He303/latest.jpg",
   ],
+
+  [
+    "",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/CONUS/EXTENT3/GOES16-CONUS-EXTENT3-625x375.gif",
+  ],
+
+
+
+
+
+  [
+    "",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/smv/AirMass/GOES16-SMV-AirMass-600x600.gif",
+  ],
+
+
+  [
+    "",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/GM/Sandwich/GOES16-GM-Sandwich-1000x1000.gif", ".8",
+  ],
+
+  [
+    "",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/smv/EXTENT3/GOES16-SMV-EXTENT3-600x600.gif",
+  ],
+
+
+  ["TROPICS", "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/taw/EXTENT3/GOES16-TAW-EXTENT3-900x540.gif"],
+
+
+
+
+
+
+
   ["24HR FORECAST", "https://www.wpc.ncep.noaa.gov/noaa/noaad1.gif"],
-  [
-    "48HR FORECAST",
-    "https://www.wpc.ncep.noaa.gov/noaa/noaad2.gif",
-  ],
+
   [
     "72HR FORECAST",
     "https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif",
@@ -98,5 +119,5 @@ const aIMG = [
     "https://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544",
     "https://www.heavens-above.com/orbitdisplay.aspx?icon=default&width=600&height=300&mode=M&satid=44909",
   ],
- 
+
 ];
