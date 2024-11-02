daa1aa22f2c18b322bd374456c57d7d9ca6ea746
bamaone251 <63915412+bamaone251@users.noreply.github.com>
2024-11-01 19:24:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ const aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 const aIMG = [
-  ["", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
+  ["", "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/DayNightCloudMicroCombo/GOES16-CONUS-DayNightCloudMicroCombo-625x375.gif"],
   [
     "",
     "https://youtu.be/N5wWOVOTHmQ",
