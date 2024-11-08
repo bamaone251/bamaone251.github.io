b61c0dbfd9dd9a5174ee576017811981dfb93b24
bamaone251 <63915412+bamaone251@users.noreply.github.com>
2024-11-08 01:28:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,23 +8,20 @@ const aURL = [
   ["add10d", "BACK", "#", "1", "R"],
   ["ff9100", "Refresh", "#", "1"],
 
-  ["2196F3", "WINDY", "https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=%C2%B0F&metricWind=default&zoom=5&overlay=radar&product=radar&level=surface&lat=30.532&lon=-87.5452", "7"],
-   ["2196F3", "PLANES", "https://globe.adsbexchange.com", "1", "R"],
+  ["2196F3", "WINDY", "https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=%C2%B0F&metricWind=default&zoom=5&overlay=radar&product=radar&level=surface&lat=31.062&lon=-90.956", "7"],
+  [
+    "2196F3",
+    "PLANES",
+    "https://globe.adsbexchange.com/",
+    "1",
+  ],
   [
     "2196F3",
     "RADAR",
     "https://thurlowpa.com/FullScreenRadarStuartZoom7Statewide.html",
     "1",
   ],
-
-  
-  [
-    "2196F3",
-    "CAMS",
-    "https://bamaone251.github.io/LOCALWX/",
-    "1",
-  ],
- 
+  ["2196F3", "CAMS", "https://bamaone251.github.io/LOCALWX/", "1", "R"],
   [
     "2196F3",
     "LIGHTNING",
@@ -39,7 +36,7 @@ const aURL = [
   [
     "2196F3",
     "WEATHER",
-    "https://embed.windy.com",
+    "https://weather.cod.edu/satrad/nexrad/?parms=MOB-N0B-0-24-100-usa-ww",
     "1",
     "R",
   ],
@@ -59,40 +56,57 @@ const aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 const aIMG = [
+  ["", "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/13/GOES16-CONUS-13-625x375.gif",],
+
+  [
+  "",
+  "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/smv/EXTENT3/GOES16-SMV-EXTENT3-600x600.gif",
+],
+
+
+[
+  "",
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/GM/Sandwich/GOES16-GM-Sandwich-1000x1000.gif", 
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/gm/DayNightCloudMicroCombo/",
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/gm/GEOCOLOR/GOES16-GM-GEOCOLOR-1000x1000.gif",
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/gm/13/GOES16-GM-13-1000x1000.gif",
+],
+
   [
     "",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/GOES16-CONUS-GEOCOLOR-625x375.gif",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/Sandwich/GOES16-CONUS-Sandwich-625x375.gif",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/DayNightCloudMicroCombo/GOES16-CONUS-DayNightCloudMicroCombo-625x375.gif",
-    ""
-  ],
+    "https://www.youtube.com/embed/N5wWOVOTHmQ", "https://www.youtube.com/embed/N5wWOVOTHmQ",
+    ],
   
+    
+
   [
-    "WARNINGS",
+    "",
+    "https://www.ssd.noaa.gov/PS/TROP/floaters/18L/imagery/rb-animated.gif",
+      ],
+
+  [
+    "",
+    "https://radar.weather.gov/ridge/standard/SOUTHMISSVLY_loop.gif",
     "https://climate.cod.edu/data/satellite/regional/southcentral/current/southcentral.ww.gif",
+    "http://wxmaps.org/pix/avnmr5.00hr.png",
   ],
-
-
   
-  ["24HR FORECAST", "https://www.wpc.ncep.noaa.gov/noaa/noaad1.gif",],
-
-  [
-    "72HR FORECAST",
-    "https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif",
-  ],
 
   [
     "",
     "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/CONUS/EXTENT3/GOES16-CONUS-EXTENT3-625x375.gif",
   ],
 
-
+  [
+  "",
+  "https://radar.weather.gov/ridge/standard/CONUS_loop.gif",
+  ],
 
 
 
   [
     "",
-    "https://www.ssd.noaa.gov/PS/TROP/floaters/18L/imagery/rb-animated.gif",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/smv/AirMass/GOES16-SMV-AirMass-600x600.gif",
   ],
 
 
@@ -109,28 +123,29 @@ const aIMG = [
 
   ["TROPICS", "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/taw/EXTENT3/GOES16-TAW-EXTENT3-900x540.gif"],
 
+[
+    "WARNINGS",
+    "https://climate.cod.edu/data/satellite/regional/southcentral/current/southcentral.ww.gif",
+  ],
 
 
 
-
-
-
+  ["24HR FORECAST", "https://www.wpc.ncep.noaa.gov/noaa/noaad1.gif",],
 
   [
-    "LIGHTNING",
-    "https://images.lightningmaps.org/blitzortung/america/index.php?animation=usa",
+    "72HR FORECAST",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif",
   ],
-  
-  [
-    "THE SUN",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/SUVI/FD/He303/latest.jpg",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/SUVI/FD/Fe171/latest.jpg",
-  ],
-  
+
   [
     "ISS & RS-44 POSITION",
     "https://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544",
     "https://www.heavens-above.com/orbitdisplay.aspx?icon=default&width=600&height=300&mode=M&satid=44909",
+  ],
+
+  [
+    "",
+    "https://www.tideschart.com/tide-charts/en/Meaher-State-Park-Mobile-Bay-Baldwin-County-Alabama-United-States-tide-chart-30030859-ft.png?",
   ],
 
 ];
